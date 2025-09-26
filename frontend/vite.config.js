5ca7c0cf191c8361346a45d8ca7c4126daa7a485
Subash1875 <subashk72005@gmail.com>
2025-09-26 16:30:11 +0530

    bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://192.168.31.28:8000",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
